00b4758026e31b0b902d83c7e1eb5418e55099a0
DarkFantast <52702535+DarkFantast@users.noreply.github.com>
2021-10-22 17:56:26 +0300

    All Project

---
+++ level-designer-task-1.1.0/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player; // тут объект игрока
+    private Vector3 offset; 
+
+    void Start () 
+    {
+        player = GameObject.FindWithTag ("Player");        
+        offset = transform.position - player.transform.position;
+    }
+
+    void FixedUpdate () 
+    {        
+        transform.position = player.transform.position + offset;
+    }
+
+}
